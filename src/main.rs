f7b7153e3bafcf43cd0fe3f4f4cd6fbefcb0aca7
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-04-05 19:37:47 -0400

    feat: pass in args through Config implemetation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use bootleg_grep;
 use std::env;
+use std::process;
+
 fn main() {
     // handle input arguments from cli
     let args: Vec<String> = env::args().collect();
+
+    let config: bootleg_grep::Config =
+        bootleg_grep::Config::configure(&args).unwrap_or_else(|error| {
+            println!("Error: {error}");
+            process::exit(1);
+        });
 
 }
