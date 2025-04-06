cd6259d25353fe3fddb91ff28ea84373a21d8cdd
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-04-05 23:35:12 -0400

    feat: created search function

--- src/lib.rs
+++ src/lib.rs
@@ -63,3 +63,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+// case sensitive searching
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in content.lines() {
+        if line.contains(query) {
+            // result.push(line);
+            result.push(line);
+        }
+    }
+
+    result
+}
+
