2f28f3ef096233719f08587131163f9fdbbfe453
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-04-05 20:15:52 -0400

    feat: implemented function to map each args to their respective property

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,17 @@ pub struct Config {
     file_path: String,
 }
 
+impl Config {
+    // configure structure properties to use in other functions
+    pub fn configure(args: &[String]) -> Result<Config, &str> {
+
+        let query_arg: String = args[1].clone();
+        let file_path_arg: String = args[2].clone();
+
+        Ok(Config {
+            query: query_arg,
+            file_path: file_path_arg,
+        })
+    }
+}
+
